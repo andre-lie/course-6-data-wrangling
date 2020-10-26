0e55e5d23693c4a1440b9c2f41464dd57f7e59f6
Andre Lie <andretw@yahoo.com>
2020-10-26 21:25:09 +1100

    R script for Paths and Working Directory lesson

---
+++ 1-path-working-directory.R
@@ -0,0 +1,20 @@
+# see working directory
+getwd()
+
+# change your working directory
+setwd()
+
+# set path to the location for raw data files in the dslabs package and list files
+path <- system.file("extdata", package="dslabs")
+list.files(path)
+
+# generate a full path to a file
+filename <- "murders.csv"
+fullpath <- file.path(path, filename)
+fullpath
+
+# copy file from dslabs package to your working directory
+file.copy(fullpath,getwd())
+
+# check if the file exists
+file.exists(filename)
